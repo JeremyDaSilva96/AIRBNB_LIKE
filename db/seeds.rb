422196c669e62e2bd45e0223dc230204008363d8
jeremy da silva <jeremydasilva1@icloud.com>
2024-06-23 18:44:41 +0200

    Cloudinary set up and working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
 require 'unsplash'
 require 'faker'
+require 'open-uri'
 
 Unsplash.configure do |config|
-  config.application_access_key = "SxuD5Sgr3OdOjdKqEAkSpBlik7WZascJSlmzmVGkZh4"
-  config.application_secret = "gBfRKVFhW5a_Ae5822Phrn2z59r8zRO8Tv3x-kFGix0"
+  config.application_access_key = ENV['UNSPLASH_ACCESS_KEY']
+  config.application_secret = ENV['UNSPLASH_SECRET_KEY']
   config.utm_source = "AIRBNB_LIKE"
 end
 
 Flat.destroy_all
 
-photos = Unsplash::Photo.search("apartment", 1, 6) # Search for 6 apartment photos
+photos = Unsplash::Photo.search("apartment", 1, 6)
 
 6.times do |i|
-  Flat.create!(
+  flat = Flat.new(
     title: Faker::Lorem.sentence(word_count: 3),
     address: Faker::Address.street_address,
     city: Faker::Address.city,
@@ -20,7 +21,12 @@ photos = Unsplash::Photo.search("apartment", 1, 6) # Search for 6 apartment phot
     country: Faker::Address.country,
     description: Faker::Lorem.paragraph,
     price_per_night: rand(50..200),
-    number_of_guests: rand(1..5),
-    image_url: photos[i].urls.raw
+    number_of_guests: rand(1..5)
   )
+
+  image_url = photos[i].urls.raw
+  image_file = URI.open(image_url)
+  flat.photo.attach(io: image_file, filename: "photo_#{i}.jpg", content_type: 'image/jpeg')
+
+  flat.save!
 end
