13b8033f163e4883b38d9878a7b123a5ce1eb72f
jeremy da silva <jeremydasilva1@icloud.com>
2024-06-23 20:18:24 +0200

    Cloudinary set to has_one photo

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,8 +1,9 @@
 class Flat < ApplicationRecord
   has_many :bookings
+  has_one_attached :photo
   # has_many :reviews, dependent: :destroy
   # has_many :users, through: :bookings
-  has_one_attached :photo
 
-  validates :title, :city, :country, :zip_code, :price_per_night, :number_of_guests, :address, :description, :photo, presence: true
+  validates :title, :city, :country, :zip_code, :price_per_night, :number_of_guests, :address, :description, presence: true
+  validates :photo, presence: true
 end
