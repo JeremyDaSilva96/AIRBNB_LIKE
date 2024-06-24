a75d2c9506bdc034524df54cdf3ed3aef43c4089
jeremy da silva <jeremydasilva1@icloud.com>
2024-06-24 09:45:19 +0200

    model booking created and association&validation done

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,8 +1,8 @@
 class Flat < ApplicationRecord
   has_many :bookings
+  has_many :users, through: :bookings
   has_many_attached :photos
   # has_many :reviews, dependent: :destroy
-  # has_many :users, through: :bookings
 
   validates :title, :city, :country, :zip_code, :price_per_night, :number_of_guests, :address, :description, presence: true
 end
