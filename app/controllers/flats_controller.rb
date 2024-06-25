2bf46e4085ff2150dca922cf43b6cad76762b374
jeremy da silva <jeremydasilva1@icloud.com>
2024-06-25 16:15:07 +0200

    Mapbox set up

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,6 +3,15 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {flat: flat}),
+        marker_html: render_to_string(partial: "marker"),
+        # marker_html: render_to_string(partial: "marker", locals: {flat: flat})
+      }
+    end
     if params[:query].present?
       sql_subquery = <<-SQL
         title ILIKE :query OR
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_135057) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_100756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_135057) do
     t.integer "zip_code"
     t.string "country"
     t.string "photos"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
