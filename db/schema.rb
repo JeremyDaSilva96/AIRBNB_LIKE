fe1fa39db64f948a1b5c8577c307ac91691eca43
jeremy da silva <jeremydasilva1@icloud.com>
2024-06-29 11:45:38 +0200

    city, zip_code, country column removed from flat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_100756) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_29_094209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,9 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_100756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
-    t.string "city"
-    t.integer "zip_code"
-    t.string "country"
     t.string "photos"
     t.float "latitude"
     t.float "longitude"
